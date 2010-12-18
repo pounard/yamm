b2f0fce8f17f313942b3b759c5b25e008201c0a4
pounard <pounard@processus.org>
2010-12-18 14:03:19 +0100

    Fixed wrong identation in file (tabs instead of spaces)

--- yamm_api/lib/Yamm/Server.php
+++ yamm_api/lib/Yamm/Server.php
@@ -6,46 +6,46 @@
  */
 class Yamm_Server
 {
-	/**
+  /**
    * Canonical URL of server Drupal root.
    * 
    * @var string
-	 */
-	protected $_url;
+   */
+  protected $_url;
 
-	/**
-	 * Get server canonical URL.
-	 * 
-	 * @return string
-	 */
-	public function getUrl() {
-		return $this->_url;
-	}
+  /**
+   * Get server canonical URL.
+   * 
+   * @return string
+   */
+  public function getUrl() {
+    return $this->_url;
+  }
 
-	/**
-	 * Human name.
-	 * 
-	 * @var string
-	 */
-	protected $_name;
+  /**
+   * Human name.
+   * 
+   * @var string
+   */
+  protected $_name;
 
-	/**
-	 * Get server human name.
-	 */
-	public function getName() {
-		return $this->_name;
-	}
+  /**
+   * Get server human name.
+   */
+  public function getName() {
+    return $this->_name;
+  }
 
-	/**
-	 * Default constructor.
-	 * 
+  /**
+   * Default constructor.
+   * 
    * @param string $url
    *   Server canonical URL.
-	 * @param string $name
-	 *   Server human name.
-	 */
-	public function __construct($url, $name) {
-		$this->_url = $url;
-		$this->_name = $name;
-	}
+   * @param string $name
+   *   Server human name.
+   */
+  public function __construct($url, $name) {
+    $this->_url = $url;
+    $this->_name = $name;
+  }
 }
